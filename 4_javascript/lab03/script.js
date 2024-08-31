480101916393a2f5e34e1897bfe5143ab1222e8c
Italo Pinheiro <pinheiroitalo2004@gmail.com>
2024-08-30 20:34:56 -0400

    concluindo lab03 de javascript

--- 4_javascript/lab03/script.js
+++ 4_javascript/lab03/script.js
@@ -16,7 +16,7 @@ function gerarRandom(min, max){
    return num
 }
 
-/**Função que representa Bola
+/**Funções (classes) que representa Bola, Triangulo e Retangulo
  * Possui como entrada as coordenadas referência no canvas (x, y)
  * E também a velocidade horizontal e vertical (velX, velY)
  * Cor e tamanho (cor e tam)
@@ -31,6 +31,53 @@ class Bola {
       this.tam = tam;
    }
 }
+class Triangulo {
+   constructor (x, y, x2, y2, x3, y3, cor, velX, velY){
+      this.x = x;
+      this.y = y;
+      this.x2=x2;
+      this.x3=x3;
+      this.y2=y2;
+      this.y3=y3;
+      this.cor = cor;
+      this.velX = velX;
+      this.velY = velY;
+   }
+ }
+
+ class Retangulo {
+   constructor (x, y, width, height, velX, velY, cor){
+      this.x = x;
+      this.y = y;
+      this.height = height;
+      this.width = width;
+      this.velX = velX;
+      this.velY = velY;
+      this.cor = cor;
+   }
+ }
+/**Funções de desenho e atualização de Retângulo */
+ Retangulo.prototype.draw = function (){
+   ctx.beginPath();
+   ctx.fillStyle = this.cor;
+   ctx.rect(this.x, this.y, this.width, this.height);
+   ctx.fill();
+ }
+
+ Retangulo.prototype.update = function() {
+   if (this.x <= 0 || this.x + this.width >= width) {
+       this.velX = -this.velX;
+   }
+
+   if (this.y <= 0 || this.y + this.height >= height) {
+       this.velY = -this.velY; 
+   }
+
+   this.x += this.velX;
+   this.y += this.velY;
+};
+
+/**Funções de desenho e atualização de Bola */
 
 Bola.prototype.draw = function() {
    ctx.beginPath();
@@ -61,32 +108,131 @@ Bola.prototype.update = function () {
    this.y += this.velY;
  };
 
+Bola.prototype.collide = function(){
 
-let balls = [];
+}
+/**Funções de desenho e atualização de Triangulo */
+ Triangulo.prototype.draw = function() {
+   ctx.beginPath();
+   ctx.fillStyle = this.cor;
+   ctx.moveTo(this.x, this.y);
+   ctx.lineTo(this.x2, this.y2);
+   ctx.lineTo(this.x3, this.y3);
+   ctx.closePath();
+   ctx.fill();
+   ctx.stroke();
+};
 
-while (balls.length < 150) {
-  let tamBolas = gerarRandom(10, 20);
+ Triangulo.prototype.update = function() {
+   const minX = Math.min(this.x, this.x2, this.x3);
+   const maxX = Math.max(this.x, this.x2, this.x3);
+   const minY = Math.min(this.y, this.y2, this.y3);
+   const maxY = Math.max(this.y, this.y2, this.y3);
+
+   if (minX <= 0 || maxX >= width) {
+       this.velX = -this.velX;
+   }
+
+   if (minY <= 0 || maxY >= height) {
+       this.velY = -this.velY;
+   }
+
+   this.x += this.velX;
+   this.x2 += this.velX;
+   this.x3 += this.velX;
+
+   this.y += this.velY;
+   this.y2 += this.velY;
+   this.y3 += this.velY;
+};
+
+
+let objects = [];
+
+
+
+while (objects.length < 20) {
+  let tamBolas = gerarRandom(10, 30);
   let ball = new Bola(
     gerarRandom(0 + tamBolas, width - tamBolas),
     gerarRandom(0 + tamBolas, height - tamBolas),
-    gerarRandom(-3, 10),
-    gerarRandom(-3, 10),
+    gerarRandom(-5, 10),
+    gerarRandom(-5, 10),
     `rgb(${gerarRandom(0, 255)}, ${gerarRandom(0, 255)}, ${gerarRandom(0, 255)})`,
     tamBolas,
   );
-  balls.push(ball);
+  objects.push(ball);
 }
 
+while (objects.length < 40) {
+   let tamTriangulos = gerarRandom(20, 40);
+   let velX = gerarRandom(-3, 3);
+   let velY = gerarRandom(-3, 3);
+   var x = gerarRandom(0, width);
+   var y = gerarRandom(0, height);
+
+   var x2 = x + tamTriangulos;
+   var y2 = y;
+
+   var x3 = x + tamTriangulos / 2;
+   var y3 = y + Math.sqrt(tamTriangulos ** 2 - (tamTriangulos / 2) ** 2); 
+
+   let triangulo = new Triangulo(
+       x,
+       y,
+       x2,
+       y2,
+       x3,
+       y3,
+       `rgb(${gerarRandom(0, 255)}, ${gerarRandom(0, 255)}, ${gerarRandom(0, 255)})`,
+       velX,
+       velY,
+   );
+   objects.push(triangulo);
+}
+
+while (objects.length < 60){
+   let widthRectangle = gerarRandom(20, 40);
+   let heightRectangle = gerarRandom(20, 40);
+   let velX = gerarRandom(-5, 10);
+   let velY = gerarRandom(-5, 10);
+   let x = gerarRandom(0, width);
+   let y = gerarRandom(0, height);
+   let ret = new Retangulo(x,y, widthRectangle, heightRectangle, velX, velY, 
+      `rgb(${gerarRandom(0, 255)}, ${gerarRandom(0, 255)}, ${gerarRandom(0, 255)})`);
+
+   objects.push(ret);
+}
+Bola.prototype.collide = function () {
+      for (let j = 0; j < objects.length; j++) {
+         if (!(this === objects[j])) {
+           const dx = this.x - objects[j].x;
+           const dy = this.y - objects[j].y;
+           const distancia = Math.sqrt(dx * dx + dy * dy);
+     
+           if (distancia < this.tam + objects[j].tam) {
+             objects[j].cor = this.cor =
+             `rgb(${gerarRandom(0, 255)}, ${gerarRandom(0, 255)}, ${gerarRandom(0, 255)})`;
+           }
+         }
+       }
+ 
+ };
 function loop() {
    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
    ctx.fillRect(0, 0, width, height);
- 
-   for (let i = 0; i < balls.length; i++) {
-     balls[i].draw();
-     balls[i].update();
-   }
- 
-   requestAnimationFrame(loop);
- }
 
- loop();
+   for (let i = 0; i < objects.length; i++) {
+       objects[i].draw();
+       objects[i].update();
+       if(objects[i] instanceof Bola){
+         objects[i].collide();
+       }
+       
+       
+   }
+
+   requestAnimationFrame(loop);
+}
+
+loop();
--- README.md
+++ README.md
@@ -11,21 +11,39 @@ Cada pasta é de um módulo
 │   ├───html3
 │   │   └───imgs
 │   └───html4
-└───3_css
-    ├───css1
-    ├───css2
-    │   ├───css_border
-    │   ├───css_box_model
-    │   ├───css_height_width
-    │   ├───css_how_to
-    │   ├───css_margin
-    │   ├───css_padding
-    │   └───css_selectors
-    ├───css3
-    │   └───imgs
-    ├───css4
-    ├───css5
-    └───css6
-        ├───.vscode
-        └───css
+├───3_css
+│   ├───css1
+│   ├───css2
+│   │   ├───css_border
+│   │   ├───css_box_model
+│   │   ├───css_height_width        
+│   │   ├───css_how_to
+│   │   ├───css_margin
+│   │   ├───css_padding
+│   │   └───css_selectors
+│   ├───css3
+│   │   └───imgs
+│   ├───css4
+│   ├───css5
+│   └───css6
+│       ├───.vscode
+│       └───css
+├───4_javascript
+│   ├───desafio_1
+│   ├───lab01
+│   ├───lab02
+│   │   └───gallery-start
+│   │       └───images
+│   └───lab03
+├───5_dom
+│   ├───lista_dom1
+│   │   ├───questao_1
+│   │   ├───questao_2
+│   │   ├───questao_3
+│   │   ├───questao_4
+│   │   └───questao_5
+│   └───lista_dom2
+└───6_bootstrap
+    └───wa-loja-bootstrap-main      
+        └───assets
    ```
