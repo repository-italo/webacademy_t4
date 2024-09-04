0cef757605f4bc9012d443e595ff022363975bf9
Italo <pinheiroitalo2004@gmail.com>
2024-09-03 20:49:21 -0400

    concluindo node1

--- 7_node/node1/index.js
+++ 7_node/node1/index.js
@@ -1,17 +1,29 @@
 const fs = require('fs')
-
+var arquivos = [];
 process.argv.forEach((val, index) => {
     if(index > 1){
         fs.readdir(val, (err, files) => {
             if(err){
                 console.log(err)
             }else{
-                console.log(`Arquivos da pasta ${val}:`);
                 files.forEach((file) => {
-                    console.log(file);
+                    arquivos.push(file);
                 })
             }
 
         })
     }
 })
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8" })
+    arquivos.forEach((e) => {
+        res.write(e);
+        res.write('<br>')
+    })
+    res.end();
+})
+
+server.listen(5333);
