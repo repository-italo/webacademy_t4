521ca4d0210b6b74c66221297769a3bfa403548c
Italo Pinheiro <pinheiroitalo2004@gmail.com>
2024-09-06 17:36:47 -0400

    finalizando trab02 de bootstrap

--- 7_node/node1/index.js
+++ 7_node/node1/index.js
@@ -1,4 +1,8 @@
-const fs = require('fs')
+const fs = require('fs');
+require('dotenv').config();
+
+const PORT = process.env.PORT ?? 3333;
+
 var arquivos = [];
 process.argv.forEach((val, index) => {
     if(index > 1){
@@ -6,9 +10,7 @@ process.argv.forEach((val, index) => {
             if(err){
                 console.log(err)
             }else{
-                files.forEach((file) => {
-                    arquivos.push(file);
-                })
+                files.forEach(file =>  arquivos.push(file));
             }
 
         })
@@ -21,9 +23,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8" })
     arquivos.forEach((e) => {
         res.write(e);
-        res.write('<br>')
+        res.write('<br>');
     })
     res.end();
 })
 
-server.listen(5333);
+server.listen(PORT, () => {
+   console.log(`Listening on PORT ${PORT}`);
+});
