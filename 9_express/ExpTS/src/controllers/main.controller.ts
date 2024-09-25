5533d63cc3e65c22f0161d78ce3e283c13bad8a2
Italo <pinheiroitalo2004@gmail.com>
2024-09-24 20:52:08 -0400

    express - aula 3

---
+++ 9_express/ExpTS/src/controllers/main.controller.ts
@@ -0,0 +1,7 @@
+
+import { Request, Response } from "express";
+
+
+const hello = (request: Request, response: Response) => {
+    
+}
---
+++ 9_express/ExpTS/src/server.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction } from "express"
+import { Request, Response } from "express"
+import { validateEnv } from "./utils/validateEnv";
+import dotenv from "dotenv"
+import morgan from "morgan";
+
+dotenv.config();
+validateEnv();
+type logFormat = "simples" | "completo"
+const app = express()
+const PORT = process.env.PORT ?? 5566
+
+
+
+app.use((req: Request , res: Response, next: NextFunction) => {
+   console.log(new Date(Date.now()))
+   console.log(req.url);
+   console.log(req.method);
+   console.log(req.httpVersion);
+   console.log(req.get('User-Agent'))
+   return next();
+});
+app.get("/", (req: Request, res: Response) => {
+   res.send("Hello World")
+});
+ 
+app.listen(PORT, () => {
+   console.log(`Listening in port ${PORT}`)
+})
